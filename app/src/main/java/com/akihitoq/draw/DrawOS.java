d6484339f802aac28d3057423e76bdcc31a943ce
Supavit Panyafang <supavitp59@email.nu.ac.th>
2019-03-21 16:04:51 +0700

    Retangle

--- app/src/main/java/com/akihitoq/draw/DrawOS.java
+++ app/src/main/java/com/akihitoq/draw/DrawOS.java
@@ -2,9 +2,11 @@ package com.akihitoq.draw;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.PointF;
+import android.graphics.Rect;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
@@ -16,6 +18,7 @@ public class DrawOS extends View {
     Paint paint = new Paint();
     PointF pointF;
     ArrayList<PointF> lpointf = new ArrayList<>();
+    ArrayList<ArrayList<PointF>> listOfLine = new ArrayList<>();
     public DrawOS(Context context) {
         super(context);
     }
@@ -27,10 +30,22 @@ public class DrawOS extends View {
         paint.setStrokeWidth(20);
         //canvas.drawLine(0,0,100,100, paint);
         //canvas.drawLine(100,0,0,100, paint);
-        if (pointF != null)
-            for(int i = 0; i <lpointf.size() ; i++ ){
-                canvas.drawCircle(lpointf.get(i).x,lpointf.get(i).y,100,paint);
+//            for (int l1 = 0; l1 < listOfLine.size() ; l1++){
+//                for (int l2 = 0; l2 < listOfLine.get(l1).size() -1 ; l2++){
+//                    canvas.drawLine(listOfLine.get(l1).get(l2).x, listOfLine.get(l1).get(l2).y,
+//                                    listOfLine.get(l1).get(l2+1).x, listOfLine.get(l1).get(l2+1).y, paint);
+//                }
+//            }
+            for (int l1 = 0; l1 < listOfLine.size(); l1++){
+                Rect rect = new Rect();
+                rect.left = (int) listOfLine.get(l1).get(0).x;
+                rect.top = (int) listOfLine.get(l1).get(0).y;
+                rect.right = (int) listOfLine.get(l1).get(listOfLine.get(l1).size()-1).x;
+                rect.bottom = (int) listOfLine.get(l1).get(listOfLine.get(l1).size()-1).y;
+                paint.setColor(new Color().BLUE);
+                canvas.drawRect(rect,paint);
             }
+
 
     }
 
@@ -42,16 +57,20 @@ public class DrawOS extends View {
             case MotionEvent.ACTION_DOWN: {
                 pointF = new PointF(x, y);
                 lpointf = addPointToList(lpointf, pointF);
+                listOfLine.add(lpointf);
                 this.invalidate();
                 break;
             }
             case MotionEvent.ACTION_MOVE: {
                 pointF = new PointF(x, y);
                 lpointf = addPointToList(lpointf, pointF);
+                listOfLine.remove(listOfLine.size()-1);
+                listOfLine.add(lpointf);
                 this.invalidate();
                 break;
             }
             case MotionEvent.ACTION_UP: {
+                listOfLine.add(lpointf);
                 lpointf = new ArrayList<>();
                 this.invalidate();
                 break;
@@ -63,10 +82,8 @@ public class DrawOS extends View {
     }
 
     private ArrayList<PointF> addPointToList(ArrayList<PointF> lpointf, PointF pointF){
-        if (lpointf.size() >= 5) {
-            lpointf.remove(0);
-            lpointf.add(pointF);
-        }else{
+        if (lpointf.size() >= 0) {
+            //lpointf.remove(0);
             lpointf.add(pointF);
         }
         return lpointf;
